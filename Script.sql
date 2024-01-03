2e0543061a096dd17f8e697effa6c6aa505f353d
Segundo Mendez <segundo.mendez@globant.com>
2024-01-03 15:03:07 -0300

    Se modifica el script y la tabla user_passwords

--- Script.sql
+++ Script.sql
@@ -3,6 +3,11 @@ CREATE DATABASE PASSWORDS;
 USE PASSWORDS;
 
 CREATE TABLE USER_PASSWORDS (
-    uuid  VARCHAR(36) PRIMARY KEY,
+    id INTEGER PRIMARY KEY,
+    uuid  VARCHAR(36) NOT NULL,
     password_hash VARCHAR(255) NOT NULL
 );
+
+-- Insert de ejemplo
+
+INSERT INTO `passwords`.`user_passwords` (`id`, `uuid`, `password_hash`) VALUES ('1', 'asd', 'asd');
